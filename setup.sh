c9f50b5fa5d5e6d1bf6755c69468d0f0526e5a47
Romain SANCHEZ <rosanche@e1r5p18.42.fr>
2020-07-21 17:59:08 +0200

    All done and setup improved

--- setup.sh
+++ setup.sh
@@ -10,30 +10,19 @@ sed_configs_back () {
     sleep 1
 }
 
+build_apply () {
+    docker build -t services/$1 srcs/$1
+    sleep 1
+    kubectl apply -f srcs/$1/$1.yml
+}
+
 
 # Install and verify that docker is running
-# bash init_docker.sh
-# docker-machine create default
-# docker-machine start
-# eval $(docker-machine env default)
-# if [[ $? == 0 ]]
-# then
-#    printf "Docker is running\n"
-# else
-#    printf "Docker can't run\n"
-#    exit
-# fi
+docker-machine create default
+docker-machine start
 
 # # # Install and launch minikube
-# minikube delete
-# rm -rf ~/.minikube
-# # mkdir -p /goinfre/${USER}/.minikube
-# # ln -sf /goinfre/${USER}/.minikube ~/.minikube
-# # chmod u+s /Users/rosanche/.minikube/bin/docker-machine-driver-hyperkit 
-# # chown root:wheel /Users/rosanche/.minikube/bin/docker-machine-driver-hyperkit && sudo chmod u+s /Users/rosanche/.minikube/bin/docker-machine-driver-hyperkit
 export MINIKUBE_HOME=/goinfre/${USER}/
-# export MINIKUBE_HOME=/tmp
-# minikube start --vm-driver=virtualbox
 minikube start driver=virtualbox --bootstrapper=kubeadm
 
 if [[ $? == 0 ]]
@@ -50,25 +39,25 @@ fi
 
 # Minikube IP
 MINIKUBE_IP=`minikube ip`
-sed_list="srcs/telegraf/telegraf.conf"
+sed_list="srcs/telegraf/telegraf.conf srcs/ftps/setup.sh srcs/mysql/wordpress.sql"
 
-# # File configuration
-# for name in $sed_list
-# do
-#     sed_configs $MINIKUBE_IP $name
-# done
-
-# sed -i.bak 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' srcs/telegraf/telegraf.conf
+# File configuration
+for name in $sed_list
+do
+    sed_configs $MINIKUBE_IP $name
+done
 
 # Delete all pods
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl delete -f srcs/nginx/nginx.yml
-kubectl delete -f srcs/mysql/my-sql.yml
+kubectl delete -f srcs/mysql/mysql.yml
 kubectl delete -f srcs/wordpress/wordpress.yml
 kubectl delete -f srcs/phpmyadmin/phpmyadmin.yml
 kubectl delete -f srcs/ftps/ftps.yml
 kubectl delete -f srcs/grafana/grafana.yml
+kubectl delete -f srcs/influxdb/influxdb.yml
+kubectl delete -f srcs/telegraf/telegraf.yml
 
 
 # Install metallb
@@ -76,48 +65,20 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-# Build container
-docker build -t services/nginx srcs/nginx/
-sleep 1
-docker build -t services/mysql srcs/mysql/
-sleep 1
-docker build -t services/wordpress srcs/wordpress/
-sleep 1
-docker build -t services/phpmyadmin srcs/phpmyadmin/
-sleep 1
-# docker build -t services/ftps srcs/ftps/
-# sleep 1
-docker build -t services/influxdb srcs/influxdb/
-sleep 1
-docker build -t services/telegraf srcs/telegraf/
-sleep 1
-docker build -t services/grafana srcs/grafana/
-sleep 1
+services="nginx mysql wordpress phpmyadmin ftps influxdb telegraf grafana"
 
-# Apply pods and services
-kubectl apply -f srcs/nginx/nginx.yml
-sleep 1
-kubectl apply -f srcs/mysql/my-sql.yml
-sleep 1
-kubectl apply -f srcs/wordpress/wordpress.yml
-sleep 1
-kubectl apply -f srcs/phpmyadmin/phpmyadmin.yml
-sleep 1
-# kubectl apply -f srcs/ftps/ftps.yml
-# sleep 1
-kubectl apply -f srcs/influxdb/influxdb.yml
-sleep 1
-kubectl apply -f srcs/telegraf/telegraf.yml
-sleep 1
-kubectl apply -f srcs/grafana/grafana.yml
-sleep 1
 kubectl apply -f srcs/metallb.yml
-sleep 1
+# build images and apply deployments
+for service in $services
+do
+    build_apply $service
+done
 
-# for name in $sed_list
-# do
-#     sed_configs_back $MINIKUBE_IP $name
-# done
+# File deconfiguration
+for name in $sed_list
+do
+    sed_configs_back $MINIKUBE_IP $name
+done
 
 # Start dashboard
 # minikube dashboard &
--- update.sh
+++ update.sh
@@ -1,13 +1,36 @@
-MINIKUBE_IP=`minikube ip`
+#! /bin/bash
 
-sed -i.bak 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' srcs/telegraf/telegraf.conf
+sed_configs () {
+    sed -i.bak 's/MINIKUBE_IP/'"$1"'/g' $2
     sleep 1
+}
+
+sed_configs_back () {
+    sed -i.bak "s/$1/""MINIKUBE_IP"'/g' $2
+    sleep 1
+}
+
+build_apply () {
+    docker build -t services/$1 srcs/$1
+    sleep 1
+    kubectl apply -f srcs/$1/$1.yml
+}
+
+# Minikube IP
+MINIKUBE_IP=`minikube ip`
+sed_list="srcs/telegraf/telegraf.conf srcs/ftps/setup.sh srcs/mysql/wordpress.sql"
+
+# File configuration
+for name in $sed_list
+do
+    sed_configs $MINIKUBE_IP $name
+done
 
 # Delete all pods
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl delete -f srcs/nginx/nginx.yml
-kubectl delete -f srcs/mysql/my-sql.yml
+kubectl delete -f srcs/mysql/mysql.yml
 kubectl delete -f srcs/wordpress/wordpress.yml
 kubectl delete -f srcs/phpmyadmin/phpmyadmin.yml
 kubectl delete -f srcs/ftps/ftps.yml
@@ -21,43 +44,17 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-# Build container
-docker build -t services/nginx srcs/nginx/
-sleep 1
-docker build -t services/mysql srcs/mysql/
-sleep 1
-docker build -t services/wordpress srcs/wordpress/
-sleep 1
-docker build -t services/phpmyadmin srcs/phpmyadmin/
-sleep 1
-# docker build -t services/ftps srcs/ftps/
-# sleep 1
-docker build -t services/influxdb srcs/influxdb/
-sleep 1
-docker build -t services/telegraf srcs/telegraf/
-sleep 1
-docker build -t services/grafana srcs/grafana/
-sleep 1
+services="nginx mysql wordpress phpmyadmin ftps influxdb telegraf grafana"
 
-# Apply pods and services
-kubectl apply -f srcs/nginx/nginx.yml
-sleep 1
-kubectl apply -f srcs/mysql/my-sql.yml
-sleep 1
-kubectl apply -f srcs/wordpress/wordpress.yml
-sleep 1
-kubectl apply -f srcs/phpmyadmin/phpmyadmin.yml
-sleep 1
-# kubectl apply -f srcs/ftps/ftps.yml
-# sleep 1
-kubectl apply -f srcs/influxdb/influxdb.yml
-sleep 1
-kubectl apply -f srcs/telegraf/telegraf.yml
-sleep 1
-kubectl apply -f srcs/grafana/grafana.yml
-sleep 1
 kubectl apply -f srcs/metallb.yml
-sleep 1
+# build images and apply deployments
+for service in $services
+do
+    build_apply $service
+done
 
-# Start dashboard
-# minikube dashboard &
+# File deconfiguration
+for name in $sed_list
+do
+    sed_configs_back $MINIKUBE_IP $name
+done
